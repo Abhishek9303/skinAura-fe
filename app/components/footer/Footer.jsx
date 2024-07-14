40a8e65605b890ffe8e6b37f5fd9cf1133d8ea3b
Abhishek <abhishekvishwakarma3244@gmail.com>
2024-07-14 18:38:07 +0530

    footer added in layout

---
+++ app/components/footer/Footer.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import Link from 'next/link';
+import { RiFacebookBoxLine, RiInstagramLine, RiMailLine, RiPhoneLine, RiYoutubeLine } from '@remixicon/react';
+
+const Footer = () => {
+  return (
+    <>
+      <div className=" bg-[#FAF7F7] lg:flex flex-row justify-center lg:justify-around items-center py-10 ">
+        <div className="flex items-center justify-around">
+          <img
+            className="lg:w-[10vw] w-[20vw]"
+            src="./images/logo.png"
+            alt="logo"
+          />
+          <div className="lg:hidden flex flex-col justify-center items-center  ">
+            <ul className="flex gap-5">
+              <li>
+                <Link href={"#"}>
+                  <RiYoutubeLine />
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"}>
+                  <RiInstagramLine />
+                </Link>
+              </li>
+              <li>
+                <Link href={"#"}>
+                  <RiFacebookBoxLine />
+                </Link>
+              </li>
+            </ul>
+            <h3 className="flex gap-2">
+              <RiPhoneLine />
+              +91 123-456-4561
+            </h3>
+          </div>
+        </div>
+        <div className="lg:block flex items-center justify-center py-5">
+          <ul className="grid grid-cols-3 gap-5">
+            <li>
+              <Link href="#">Home</Link>
+            </li>
+            <li>
+              <Link href="#">About</Link>
+            </li>
+            <li>
+              <Link href="#">Instagram</Link>
+            </li>
+
+            <li>
+              <Link href="#">Centers</Link>
+            </li>
+            <li>
+              <Link href="#">Reviews</Link>
+            </li>
+            <li>
+              <Link href="#">Facebook</Link>
+            </li>
+
+            <li>
+              <Link href="#">Services</Link>
+            </li>
+            <li>
+              <Link href="#">Products</Link>
+            </li>
+            <li>
+              <Link href="#">Youtube</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="lg:block hidden">
+          <h3>Contact Us</h3>
+          <h3>+91 123-456-4561</h3>
+          <h3 className="flex gap-2">
+            <RiMailLine />
+            skinaura@gmail.com
+          </h3>
+        </div>
+          <h2 className='text-center py-2 lg:hidden'>@ 2024 copyright All Right Reserved</h2>
+      </div>
+      <div className="py-2 bg-black text-center text-white">
+        <h3>#India’s Top Trusted Brand</h3>
+      </div>
+    </>
+  );
+}
+
+export default Footer
--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body suppressHydrationWarning>
         {children}
+        <Footer/>
       </body>
     </html>
   );
