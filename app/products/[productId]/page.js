4e0566616ed574487239502a711bd80eb20e810a
unknown <abhayagnihotri1585@gmail.com>
2024-07-25 20:15:37 +0530

    fixd-ui-of-services

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,46 +1,43 @@
 import Product from '@/app/components/product/Product';
 import ProductImages from '@/app/components/swiper/ProductImages';
 import ReviewSwiper from '@/app/components/swiper/ReviewSwiper';
+import VideoDiv from '../../components/videoDiv/VideoDiv';
 import React from 'react'
+import Button from '@/app/components/button/Button';
 
 const SingleProduct = (props) => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center ">
+      <div className="md:w-[85vmax] px-8 md:px-5 min-h-screen mx-auto">
         <div className="flex lg:flex-row flex-col py-10 items-center justify-center">
           <div className="lg:w-[35vw] w-[80vw] lg:h-[40vh] h-[40vh] ">
-              <ProductImages height={"50vh"} />;
+              <ProductImages height={"50vh"} />
           </div>
-          <div className="lg:w-[45vw] w-[80vw] lg:h-[40vh] h-[50vh]  lg:px-10 px-5 lg:py-5 py-2">
-            <h1 className="text-2xl">Skin Aura’s Glglow Cream-UNISEX</h1>
-            <p>53 people orderd last week</p>
+          <div className="md:ml-16 lg:w-[45vw] w-[80vw] lg:h-[40vh] h-[28vh]  lg:px-10 px-5 lg:py-5 py-16">
+            <h1 className="text-3xl mb-3">Skin Aura’s Glglow Cream-UNISEX</h1>
+            <p className='text-sm'>53 people orderd last week</p>
             <div>star</div>
             <h5 className="lg:text-base py-2">
               The most natural and trusted product of ours -
               (Product-Description)
             </h5>
             <div className="flex items-start justify-between">
-              <h4>MRP : ₹ 320 /-</h4>
+              <h1 className='text-3xl mt-4 font-semibold'>MRP : ₹ 320 /-</h1>
             </div>
             <div className=" flex items-center py-10 gap-5">
-              <button className="bg-[#6A4D6F] text-white lg:py-4 font-bold lg:px-10 py-2 px-3 rounded-lg">
-                Add to cart
-              </button>
-              <button className="bg-[#6A4D6F] text-white lg:py-4 font-bold lg:px-10 py-2 px-3 rounded-lg">
-                Buy now
-              </button>
+            <Button text='Add To Cart' className='rounded-lg ' />
+            <Button text='Buy Now' className='rounded-lg ' />
             </div>
           </div>
         </div>
-        <div className="flex lg:flex-row flex-col lg:text-left text-center  w-[80vw] lg:py-10 py-5 items-center justify-between">
-          <div className="lg:w-[50vw] w-[80vw]">
-            <h1 className="text-2xl py-2 lg:text-left text-center">
+        <div className="flex lg:flex-row flex-col mx-auto lg:text-left text-center  w-[80vw] lg:py-10 py-5 items-center justify-between">
+          <div className="lg:w-[40vw] w-[80vw]">
+            <h1 className="text-xl py-2 lg:text-left text-center">
               Skin Cream that prevent ageing wrinkels and improve skin health{" "}
             </h1>
-            <p className="py-2 text-sm lg:text-left text-justify">
+            <p className="py-4 text-sm lg:text-left text-justify">
               Nourishes skin internally with essential supplements with a
-              formulation of rare and effective Ayurvedic herbs. Targets poor
-              skin health, dosha imbalance, and poor blood flow. Inadequate
+               dosha imbalance, and poor blood flow. Inadequate
               sleep, diet, and lifestyle can cause an imbalance of hormones.
               Adaptogenic herbs in skin ras such as bhringraj, shatavari, and
               ashwagandha help restore that. Made with proven, safe, and natural
@@ -50,18 +47,18 @@ const SingleProduct = (props) => {
           </div>
           <div>
             <img
-              className="lg:w-[20vw] w-[80vw] py-5"
+              className="lg:w-[20vw] w-[70vw] py-12"
               src="/images/newImage.png"
               alt=""
             />
           </div>
         </div>
-        <div className="py-10">
-          <h1 className="text-[3vmax] text-center">Certified Products</h1>
-          <div className="w-[80vw] h-[60vh] my-5 rounded-2xl flex items-center justify-center bg-black"></div>
+        <div className="py-12">
+          <h1 className="md:text-[3vmax] text-[3.5vmax] font-juanaRegular mb-12 text-center">Certified Products</h1>
+          <VideoDiv text='Certificates' imgSrc='https://plus.unsplash.com/premium_photo-1713628398440-9d056ad0d468?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
         </div>
         <div className="py-10">
-          <h1 className="text-center text-[3vmax] font-juanaRegular">Our Product Results</h1>
+          <h1 className="text-center md:text-[3vmax] text-[3.5vmax] font-juanaRegular">Our Product Results</h1>
           <p className="text-sm text-center">
             we don’t say it our members say it
           </p>
@@ -70,8 +67,8 @@ const SingleProduct = (props) => {
           </div>
         </div>
         <div className='my-5'>
-          <h1 className='text-center text-[2.8vmax] mb-10'>You May Also like</h1>
-          <div className='flex lg:flex-row flex-col my-5 gap-10'>
+          <h1 className='text-center font-juanaRegular md:text-[2.8vmax] text-[3.5vmax] mb-12'>You May Also like</h1>
+          <div className='flex lg:flex-row flex-col my-5 gap-20'>
               <Product/>
               <Product/>
               <Product/>
