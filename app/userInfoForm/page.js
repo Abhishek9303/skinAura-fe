51aca034369ca6271607be15ceea0b79f7fbc084
unknown <abhayagnihotri1585@gmail.com>
2024-07-31 12:30:13 +0530

    added-contact-form

--- app/userInfoForm/page.js
+++ app/userInfoForm/page.js
@@ -2,13 +2,26 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="w-full flex items-center justify-center h-screen">
-      <div className="hidden md:block h-full w-[40%] bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1552046122-03184de85e08?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}></div>
-      <div className="flex bg-red-500 items-center justify-center w-full md:w-[60%] md:p-24">
-        <form className=" bg-white shadow-md rounded-lg p-5 md:p-8 w-full space-y-4">
-          <h1 className="text-2xl font-semibold text-center text-gray-700">Contact Information</h1>
+    <div className="w-full flex items-center justify-center lg:h-screen">
+      <div
+        className="hidden lg:block h-full w-[40%] bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1552046122-03184de85e08?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        }}
+      ></div>
+      <div className="flex items-center justify-center w-full lg:w-[60%] p-5 md:px-24">
+        <form className=" bg-white shadow-md border-[0.5px] border-[#0000003b] rounded-lg lg:p-8 p-5 w-full md:space-y-2">
+          <h1 className="text-2xl font-semibold text-center text-gray-700">
+            Contact Information
+          </h1>
           <div className="space-y-2">
-            <label htmlFor="userContact" className="block text-sm font-medium text-gray-700">Contact:</label>
+            <label
+              htmlFor="userContact"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Contact:
+            </label>
             <input
               type="text"
               id="userContact"
@@ -18,9 +31,16 @@ const Page = () => {
               className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
             />
           </div>
-          <h1 className="text-xl font-semibold text-gray-700 mt-6">Shipping Address</h1>
+          <h1 className="text-xl font-semibold text-gray-700 mt-6">
+            Shipping Address
+          </h1>
           <div className="space-y-2">
-            <label htmlFor="countries" className="block text-sm font-medium text-gray-700">Country:</label>
+            <label
+              htmlFor="countries"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Country:
+            </label>
             <select
               name="countries"
               id="countries"
@@ -45,12 +65,17 @@ const Page = () => {
             />
           </div>
           <div className="mt-4">
-            <label htmlFor="address" className="block text-sm font-medium text-gray-700">Address:</label>
+            <label
+              htmlFor="address"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Address:
+            </label>
             <textarea
               name="address"
               id="address"
               placeholder="Address"
-              className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary h-32"
+              className="w-full h-16 resize-none px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
             ></textarea>
           </div>
           <div className="space-y-2 sm:flex sm:space-y-0 sm:space-x-4 mt-4">
@@ -96,14 +121,14 @@ const Page = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 bg-[#6A4D6F] text-white font-semibold rounded-md shadow-sm hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+            className="w-full px-4 py-2 mt-4 bg-[#6A4D6F] text-white font-semibold rounded-md shadow-sm hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
           >
             Submit
           </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
