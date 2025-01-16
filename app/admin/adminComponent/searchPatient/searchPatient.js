62dff7273cbb6bbb3ab65f5cebcf571485278f21
Abhishek <abhishekvishwakarma3244@gmail.com>
2025-01-16 17:41:49 +0530

    download report

--- app/admin/adminComponent/searchPatient/searchPatient.js
+++ app/admin/adminComponent/searchPatient/searchPatient.js
@@ -7,12 +7,19 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/toast";
 import TodayEntryTable from "../entryTable/TodayEntryTable";
 import { Separator } from "@/components/ui/separator";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet"; // Import Sheet components
+
 const SearchPatient = () => {
   const { admin } = adminStore();
   const [mobileNumber, setMobileNumber] = useState("");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [showTable, setShowTable] = useState(false);
+  const [showSheet, setShowSheet] = useState(false);
+  const [date, setDate] = useState("");
+  const toast = useToast();
+  const [email, setEmail] = useState("");
+
   const handleSearch = () => {
     const config = {
       method: "get",
@@ -36,15 +43,59 @@ const SearchPatient = () => {
           });
         } else {
           setData([]);
+          toast({
+            type: "error",
+            message: "No data found",
+            variant : "destructive"
+          });
           setShowTable(false);
         }
       })
-      .catch((error) => {});
+      .catch((error) => {
+        toast({
+          type: "error",
+          message: "Failed to fetch data",
+          variant: "destructive",
+        });
+      });
+  };
+
+  const handleDownload = () => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `${process.env.BACKEND_URL}api/v1/admin/getAllTodaysEntry?startDate=${date}&download=true&emailId=${email}`,
+      headers: {
+        "auth-token": admin?.token,
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        if(response.data.success){
+          toast({
+            type: "success",
+            message: "Report downloaded successfully",
+          });
+        }
+      })
+      .catch((error) => {
+        toast({
+          type: "error",
+          message: "Failed to download report",
+          variant: "destructive",
+        });
+      })
+      .finally(() => {
+        setShowSheet(false);
+      })
   };
 
   return (
     <>
-      <div className=" flex  items-center justify-around">
+      <div className=" flex  items-center justify-between">
         <Input
           className="w-1/2"
           type="text"
@@ -52,7 +103,38 @@ const SearchPatient = () => {
           onChange={(e) => setMobileNumber(e.target.value)}
           placeholder="Enter mobile number"
         />
-        <Button onClick={handleSearch}>Search</Button>
+        <div className="flex gap-5">
+          <Button onClick={handleSearch}>Search</Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <div>
+                <Button>Download Report</Button>
+              </div>
+            </SheetTrigger>
+            <SheetContent>
+              <div className="p-4">
+                <h2 className="text-lg font-semibold">Download Report</h2>
+                <div className="mt-4">
+                  <Input
+                    type="date"
+                    placeholder="Select date"
+                    className="w-full mb-4"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                  />
+                  <Input
+                    type="email"
+                    placeholder="Enter email address"
+                    className="w-full mb-4"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Button onClick={handleDownload}>Download</Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
       <div className="mt-5">
         <Separator />
