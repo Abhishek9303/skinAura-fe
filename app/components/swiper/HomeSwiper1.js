d0848104270ec28657f2df8708880bc58c5dd83d
Abhishek Vishwakarma <92821298+Abhishek9303@users.noreply.github.com>
2026-03-08 17:26:44 +0530

    feat: Update HomeSwiper images to ImageKit, add new slides, and configure backend for local development.

--- app/components/swiper/HomeSwiper1.js
+++ app/components/swiper/HomeSwiper1.js
@@ -16,29 +16,39 @@ const HomeSwiper1 = () => {
       }}
     >
       <SplideTrack>
-        <SplideSlide className="w-[50vw]  md:h-[50vh] h-[35vmax] flex items-center justify-center">
+        <SplideSlide className="w-[50vw]  md:h-[50vh] h-[40vmax] flex items-center justify-center">
           <div className="swiperContent md:px-8 px-4 flex flex-col items-center z-10">
             <img
-              className="object-cover object-center md:w-[80%] w-full"
-              src="https://res.cloudinary.com/den1739gv/image/upload/v1743923259/creating_a_comprehensive_poster_for_a_shopping_website_featuring_multiple_skinnaa_products_on_a_whi_3d0dots76cndax8uszyn_0_bcszon.png"
+              className="object-cover object-center h-full "
+              src="https://ik.imagekit.io/9xqqzmzt4/FineAura/HomePage/Gemini_Generated_Image_87a40o87a40o87a4.webp?updatedAt=1772908452230"
               alt=""
             />
           </div>
         </SplideSlide>
-        <SplideSlide className="w-full  md:h-[50vh] h-[35vmax] flex items-center justify-center">
-          <div className="swiperContent flex items-center justify-center">
-            <img
-              className="object-cover object-center md:w-[80%] w-full"
-              src="https://res.cloudinary.com/den1739gv/image/upload/v1743924044/image_3_znojhc.jpg"
-              alt=""
-            />
-          </div>
-        </SplideSlide>
+
         <SplideSlide className="w-full md:h-[50vh] h-[35vmax] flex items-center justify-center">
           <div className="swiperContent flex items-center justify-center">
             <img
-              className="object-cover object-center md:w-[80%] w-full"
-              src="https://res.cloudinary.com/den1739gv/image/upload/v1743923909/image_1_zqmibq.jpg"
+              className="object-cover object-center h-full"
+              src="https://ik.imagekit.io/9xqqzmzt4/FineAura/HomePage/Gemini_Generated_Image_dkpiaodkpiaodkpi.webp?updatedAt=1772908452337"
+              alt=""
+            />
+          </div>
+        </SplideSlide>
+         <SplideSlide className="w-full md:h-[50vh] h-[35vmax] flex items-center justify-center">
+          <div className="swiperContent flex items-center justify-center">
+            <img
+              className="object-cover object-center h-full"
+              src="https://ik.imagekit.io/9xqqzmzt4/FineAura/HomePage/Gemini_Generated_Image_bjvrz3bjvrz3bjvr.webp?updatedAt=1772908452041"
+              alt=""
+            />
+          </div>
+        </SplideSlide>
+         <SplideSlide className="w-full md:h-[50vh] h-[35vmax] flex items-center justify-center">
+          <div className="swiperContent flex items-center justify-center">
+            <img
+              className="object-cover object-center h-full"
+              src="https://ik.imagekit.io/9xqqzmzt4/FineAura/HomePage/geminiimages.webp?updatedAt=1772908451073"
               alt=""
             />
           </div>
